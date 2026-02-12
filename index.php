a3ca353a5af9a69e4867ac6f413b0f115dc5ae04
Evando Samuel Amarilla <eamarilla@CREMINOX.local>
2026-02-12 14:34:29 -0300

    Prueba de arreglo de tiempo de carga (Windows)

--- index.php
+++ index.php
@@ -69,23 +69,6 @@ if ($mac_registered) {
     echo '<!doctype html><html lang="es"><head><meta charset="utf-8"><title>Acceso WiFi | Cremona Inoxidable</title><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>*{box-sizing:border-box;margin:0;padding:0;font-family:\'Segoe UI\',Tahoma,Geneva,Verdana,sans-serif;}body{background:#f0f2f5;display:flex;justify-content:center;align-items:center;height:100vh;}.card{background:#fff;padding:40px 30px;border-radius:15px;box-shadow:0 10px 25px rgba(0,0,0,0.1);width:100%;max-width:400px;text-align:center;}.logo{width:200px;margin-bottom:10px;}p.back-text{margin-bottom:5px;color:#333;font-size:18px;font-weight:bold;}p.back2-text{margin-bottom:0px;color:#333;font-size:14px;}</style></head><body><div class="card"><img src="Creminox.png" alt="Creminox Logo" class="logo"><p class="back-text">¡Su conexión fue establecida!</p><p class="back2-text">Ya puede navegar por la web. <br>Si no es redirigido automaticamente, puede retirarse de esta página sin problemas.</p></div></body></html>';
     exit;
 } else {
-    require __DIR__ . '/vendor/autoload.php';
-
-    $duration = 0;
-    $site_id = 'default';
-
-    $controlleruser     = 'admin';
-    $controllerpassword = 'miloj@324156';
-    $controllerurl      = 'https://192.168.20.16:8443';
-    $controllerversion  = '10.0.162';
-    $debug = false;
-
-    $unifi_connection = new UniFi_API\Client($controlleruser, $controllerpassword, $controllerurl, $site_id, $controllerversion);
-    $unifi_connection->set_debug($debug);
-    $unifi_connection->login();
-
-    $unifi_connection->unauthorize_guest($mac);
-
     if ($conn && !$conn->connect_error) {
         $conn->close();
     }
